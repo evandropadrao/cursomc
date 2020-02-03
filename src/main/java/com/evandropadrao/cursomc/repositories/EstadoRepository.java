459707febc2b7233572a7f5e1ed06b37f5bad780
Evandro Padrao <evandropadrao@gmail.com>
2020-02-03 00:42:45 -0300

    Estado e Cidade

---
+++ src/main/java/com/evandropadrao/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.evandropadrao.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.evandropadrao.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
